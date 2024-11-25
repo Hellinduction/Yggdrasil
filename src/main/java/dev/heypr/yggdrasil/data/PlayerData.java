2a004118ab04e230d1a2415137468819182a86c6
Hellinduction <joseph.connor.west@icloud.com>
2024-11-25 00:28:18 +0000

    Added PlaceholderAPI support + Fancy skin changing thing from server-side textures

--- src/main/java/dev/heypr/yggdrasil/data/PlayerData.java
+++ src/main/java/dev/heypr/yggdrasil/data/PlayerData.java
@@ -1,13 +1,29 @@
 package dev.heypr.yggdrasil.data;
 
+import dev.heypr.yggdrasil.Yggdrasil;
+import dev.heypr.yggdrasil.misc.Colors;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.util.UUID;
+
 public class PlayerData {
 
+    private UUID uuid;
     private int lives;
     private boolean isBoogeyman;
 
-    public PlayerData(int lives) {
+    public PlayerData(UUID uuid, int lives) {
+        this.uuid = uuid;
         this.lives = lives;
         this.isBoogeyman = false;
+
+        this.updateSkin();
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 
     public int getLives() {
@@ -16,10 +32,26 @@ public class PlayerData {
 
     public void addLives(int amount) {
         this.lives += amount;
+        this.updateSkin();
     }
 
     public void decreaseLives(int amount) {
         this.lives -= amount;
+        this.updateSkin();
+    }
+
+    private void updateSkin() {
+        final Player player = this.getPlayer();
+
+        if (player == null || !player.isOnline())
+            return;
+
+        final File skinFile = Colors.getSkinFile(Yggdrasil.plugin, player, this.lives);
+
+        if (skinFile == null)
+            return;
+
+        Yggdrasil.plugin.skinsManager.skin(player, skinFile);
     }
 
     public boolean isBoogeyman() {
@@ -28,5 +60,9 @@ public class PlayerData {
 
     public void setBoogeyman(boolean boogeyman) {
         this.isBoogeyman = boogeyman;
+    }
+
+    public Player getPlayer() {
+        return Bukkit.getPlayer(this.uuid);
     }
 }
